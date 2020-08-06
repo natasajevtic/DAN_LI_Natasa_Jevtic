86307175fb999b3668db2af782a3230bc53f3dc7
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-06 12:55:41 +0200

    Class for encryption created

---
+++ DAN_LI_Natasa_Jevtic/Zadatak_1/Helper/Encryption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Zadatak_1.Helper
+{
+    class Encryption
+    {
+        public string EncryptPassword(string password)
+        {
+
+            byte[] b = Encoding.UTF8.GetBytes(password);
+            string encrypted = Convert.ToBase64String(b);
+            return encrypted;
+        }
+    }
+}
