1719e8d4518a421987e0eb7358b7e95b4675d1c9
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-06 13:01:12 +0200

    Classes Users, Patients and Doctors created

---
+++ DAN_LI_Natasa_Jevtic/Zadatak_1/Models/Doctors.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Zadatak_1.Helper;
+
+namespace Zadatak_1.Models
+{
+    class Doctors
+    {
+        Encryption encryption = new Encryption();
+        /// <summary>
+        /// This method creates a list of data from view of all doctors.
+        /// </summary>
+        /// <returns>List of doctors.</returns>
+        public List<tblDoctor> GetAllDoctors()
+        {
+            try
+            {
+                using (Medical_InstitutionEntities context = new Medical_InstitutionEntities())
+                {
+                    return context.tblDoctors.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+        /// <summary>
+        /// This method adds doctor to DbSet and save changes to database.
+        /// </summary>
+        /// <param name="doctor">Doctor.</param>
+        /// <returns>True if created, false if not.</returns>
+        public bool AddDoctor(vwDoctor doctor)
+        {
+            Users users = new Users();
+            try
+            {
+                using (Medical_InstitutionEntities context = new Medical_InstitutionEntities())
+                {
+                    tblUser user = new tblUser
+                    {
+                        Name = doctor.Name,
+                        Password = encryption.EncryptPassword(doctor.Password),
+                        Surname = doctor.Surname,
+                        Username = doctor.Username,
+                        JMBG = doctor.JMBG
+                    };
+                    context.tblUsers.Add(user);
+                    context.SaveChanges();
+                    doctor.UserId = user.UserId;
+                    tblDoctor newDoctor = new tblDoctor
+                    {
+                        UserId = user.UserId,
+                        BankAccountNumber = doctor.BankAccountNumber
+                    };
+                    context.tblDoctors.Add(newDoctor);
+                    context.SaveChanges();
+                    doctor.DoctorId = newDoctor.DoctorId;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return false;
+            }
+        }
+    }
+}
---
+++ DAN_LI_Natasa_Jevtic/Zadatak_1/Models/Users.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Zadatak_1.Helper;
+
+namespace Zadatak_1.Models
+{
+    class Users
+    {
+        Encryption encryption = new Encryption();
+        /// <summary>
+        /// This method finds patient in database with forwarded username and password.
+        /// </summary>
+        /// <param name="username">Username.</param>
+        /// <param name="password">Password.</param>
+        /// <returns>Patient if finded, null if not.</returns>
+        public vwPatient FindPatient(string username, string password)
+        {
+            string encryptedPassword = encryption.EncryptPassword(password);
+            try
+            {
+                using (Medical_InstitutionEntities context = new Medical_InstitutionEntities())
+                {
+                    return context.vwPatients.Where(x => x.Username == username && x.Password == encryptedPassword).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+        /// <summary>
+        /// This method finds doctor in database with forwarded username and password.
+        /// </summary>
+        /// <param name="username">Username.</param>
+        /// <param name="password">Password.</param>
+        /// <returns>Doctor if finded, null if not.</returns>
+        public vwDoctor FindDoctor(string username, string password)
+        {
+            string encryptedPassword = encryption.EncryptPassword(password);
+            try
+            {
+                using (Medical_InstitutionEntities context = new Medical_InstitutionEntities())
+                {
+                    return context.vwDoctors.Where(x => x.Username == username && x.Password == encryptedPassword).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+        /// <summary>
+        /// This method creates a list of data from view of all users.
+        /// </summary>
+        /// <returns>List of users.</returns>
+        public List<tblUser> GetAllUsers()
+        {
+            try
+            {
+                using (Medical_InstitutionEntities context = new Medical_InstitutionEntities())
+                {
+                    return context.tblUsers.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+    }
+}
